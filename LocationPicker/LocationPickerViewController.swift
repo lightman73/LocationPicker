14e18a43b1a167e0c166df41a4285893d84c6355
Franz Marini <f.marini@opengate.biz>
2019-11-14 18:24:24 +0100

    feat: add pinColor option

--- LocationPicker/LocationPickerViewController.swift
+++ LocationPicker/LocationPickerViewController.swift
@@ -45,6 +45,8 @@ open class LocationPickerViewController: UIViewController {
     public var selectButtonTitle = "Select"
     
     public var searchBarInTitle = true
+    
+    public var pinColor: UIColor = .green
 	
 	public lazy var currentLocationButtonBackground: UIColor = {
 		if let navigationBar = self.navigationController?.navigationBar,
@@ -162,12 +164,12 @@ open class LocationPickerViewController: UIViewController {
 		mapView.addGestureRecognizer(locationSelectGesture)
 
 		// search
-        if searchBarInTitle {
+        if #available(iOS 13.0, *) && !searchBarInTitle {
+            navigationItem.searchController = searchController
+        } else {
             navigationItem.titleView = searchBar
             // http://stackoverflow.com/questions/32675001/uisearchcontroller-warning-attempting-to-load-the-view-of-a-view-controller/
             _ = searchController.view
-        } else {
-            navigationItem.searchController = searchController
         }
 		definesPresentationContext = true
 		
@@ -372,7 +374,7 @@ extension LocationPickerViewController: MKMapViewDelegate {
 		if annotation is MKUserLocation { return nil }
 		
 		let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "annotation")
-		pin.pinColor = .green
+		pin.pinTintColor = pinColor
 		// drop only on long press gesture
 		let fromLongPress = annotation is MKPointAnnotation
 		pin.animatesDrop = fromLongPress
